49dd19984c8c6ab5e0a979779113fcda0bd18814
fitforclimate-cloud <fitforclimate@gmail.com>
2026-03-03 20:47:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,27 @@
+const CACHE = "bungalows-v2"; // <-- bump versie bij wijzigingen
+
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("bungalows-v1").then((cache) =>
-      cache.addAll(["./", "./index.html", "./manifest.webmanifest", "./sw.js"])
+    caches.open(CACHE).then((cache) =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./bungalows_map.html",
+        "./manifest.webmanifest",
+        "./sw.js",
+      ])
     )
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
